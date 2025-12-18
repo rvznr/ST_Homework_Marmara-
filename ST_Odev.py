dde11ad13709d74a32fc71928d9b6dab8e000468
Ravza <161067001+rvznr@users.noreply.github.com>
2025-12-19 01:30:14 +0300

    Python odev dosyasi eklendi

---
+++ ST_Odev.py
@@ -0,0 +1,16 @@
+##Special Topics Ödev
+
+def toplama(a, b):
+    return a + b
+
+
+def carpma(a, b):
+    return a * b
+
+
+if __name__ == "__main__":
+    x = 5
+    y = 3
+
+    print("Toplama sonucu:", toplama(x, y))
+    print("Çarpma sonucu:", carpma(x, y))
